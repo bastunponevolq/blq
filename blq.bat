23409095d574f34c3e147272a7e955073446ab9f
bastunponevolq <127227330+bastunponevolq@users.noreply.github.com>
2023-03-24 00:26:27 +0200

    Update blq.bat

--- blq.bat
+++ blq.bat
@@ -1,5 +1,5 @@
 curl -LJOk "https://github.com/bastunponevolq/nware/raw/main/macro.zip"
 tar -xf macro.zip
-star C:\Users\Nware\Downloads\1\MacroRecorder.exe
+star C:\Users\nware\Downloads\1\MacroRecorder.exe
 
 
